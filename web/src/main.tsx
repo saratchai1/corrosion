c7d67ab7110ae6d8a784ad1c4a8c349521c628de
saratchai1 <saratchai@gmail.com>
2026-09-02 15:31:11 +0700

    Make 2.5m comparison the standalone web variant

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'maplibre-gl/dist/maplibre-gl.css'
-import './styles.css'
-import './navigation.css'
-import App from './App'
+import SuperResolutionPage from './SuperResolutionPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SuperResolutionPage />
   </StrictMode>,
 )
